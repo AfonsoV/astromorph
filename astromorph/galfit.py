8a9fac60f73fa15352a90eaffdf72fda7932e3f6
Bruno Ribeiro <ribeiro@itterbeek.strw.leidenuniv.nl>
2018-01-16 12:12:14 +0100

    Updated galfit module

--- astromorph/galfit.py
+++ astromorph/galfit.py
@@ -2,26 +2,76 @@ import subprocess as sp
 import astropy.io.fits as pyfits
 
 def get_fixpars_default():
+    r""" Returns the default dictionary containing the information on whether
+    or not to fix any parameter of the fit. By default, all parameters are
+    not fixed.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    fixpars : dict
+        A dictionary for each of the sersic parameters setting the fix/free key.
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
     return {'x':1,'y':1,'m':1,'re':1,'n':1,'q':1,'pa':1,'sky':1}
 
-def write_object(f,model,x,y,m,re,n,ba,pa,num,fixpars=None):
+def write_object(model,x,y,m,re,n,ba,pa,num,fixpars=None):
+    r"""
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
     if fixpars is None:
         fixpars=get_fixpars_default()
 
-    f.write("#Object number: %i\n"%num)
-    f.write(' 0) %s             # Object type\n'%model)
-    f.write(' 1) %6.4f %6.4f  %i %i    # position x, y        [pixel]\n'%(x,y,fixpars['x'],fixpars['y']))
-    f.write(' 3) %4.4f      %i       # total magnitude\n' %(m,fixpars['m']))
-    f.write(' 4) %4.4f       %i       #     R_e              [Pixels]\n'%(re,fixpars['re']))
-    f.write(' 5) %4.4f       %i       # Sersic exponent (deVauc=4, expdisk=1)\n'%(n,fixpars['n']))
-    f.write(' 9) %4.4f       %i       # axis ratio (b/a)   \n'%(ba,fixpars['q']))
-    f.write('10) %4.4f       %i       # position angle (PA)  [Degrees: Up=0, Left=90]\n'%(pa,fixpars['pa']))
-    f.write(' Z) 0                  #  Skip this model in output image?  (yes=1, no=0)\n')
-    f.write(' \n')
-    return
+    objString = ""
+    objString += "#Object number: %i\n"%(num)
+    objString += " 0) %s             # Object type\n"%(model)
+    objString += " 1) %6.4f %6.4f  %i %i    # position x, y        [pixel]\n"%(x,y,fixpars['x'],fixpars['y'])
+    objString += " 3) %4.4f      %i       # total magnitude\n"%(m,fixpars['m'])
+    objString += " 4) %4.4f       %i       #     R_e              [Pixels]\n"%(re,fixpars['re'])
+    objString += " 5) %4.4f       %i       # Sersic exponent (deVauc=4, expdisk=1)\n"%(n,fixpars['n'])
+    objString += " 9) %4.4f       %i       # axis ratio (b/a)   \n"%(ba,fixpars['q'])
+    objString += "10) %4.4f       %i       # position angle (PA)  [Degrees: Up=0, Left=90]\n"%(pa,fixpars['pa'])
+    objString += " Z) 0                  #  Skip this model in output image?  (yes=1, no=0)\n"
+    objString += " \n"
+    return objString
 
 
-def input_file(f,magzpt,sky,x_range,y_range,sconvbox,pixscale,imgname='galaxy.fits',outname="results.fits",psfname='psf.fits',maskname="none",signame='none',fixpars=None):
+def input_file(f,modelsString,magzpt,sky,x_range,y_range,sconvbox,pixscale,imgname='input.fits',outname="output.fits",psfname='none',maskname="none",signame='none',fixpars=None):
+    r"""
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
     if fixpars is None:
         fixpars=get_fixpars_default()
 
@@ -61,11 +111,7 @@ def input_file(f,magzpt,sky,x_range,y_range,sconvbox,pixscale,imgname='galaxy.fi
     f.write("# ------------------------------------------------------------------------------\n")
     f.write("\n")
 
-    obj=open('galfit_object.temp','r')
-    objects=obj.readlines()
-    for line in objects:
-        f.write(line)
-    obj.close()
+    f.write(modelsString)
 
     f.write("# Object: Sky\n")
     f.write(" 0) sky                    #  object type\n")
@@ -77,68 +123,135 @@ def input_file(f,magzpt,sky,x_range,y_range,sconvbox,pixscale,imgname='galaxy.fi
     return
 
 
-def galaxy_maker(mag_zpt,xsize,ysize,model,xc,yc,pars,theta,sky=0):
-    f=open('galfit_object.temp','w')
+def galaxy_maker(mag_zpt,xsize,ysize,model,xc,yc,pars,theta,sky=0,psfname="none"):
+    r"""
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
+
     if model=='sersic':
         mag,re,n,q = pars
-        write_object(f,model,xc,yc,mag,re,n,q,theta,1)
+        modelPars = write_object(model,xc,yc,mag,re,n,q,theta,1)
     elif model=='expdisk':
         mag_d,h,q=pars
-        write_object(f,model,xc,yc,mag_d,h,1.0,q,theta,1)
+        modelPars = write_object(f,model,xc,yc,mag_d,h,1.0,q,theta,1)
     elif model=='devauc':
         mag_b,re,q=pars
-        write_object(f,model,xc,yc,mag_b,re,4.0,q,theta,1)
+        modelPars = write_object(f,model,xc,yc,mag_b,re,4.0,q,theta,1)
     elif model=='compost':
         mag_d,rd,mag_b,rb,ba=pars
         theta_b,theta_d=theta
-        write_object(f,'expdisk',xc,yc,mag_d,rd,1.0,ba,theta_d,1)
-        write_object(f,'devauc',xc,yc,mag_b,rb,4.0,ba,theta_b,2)
+        modelPars = write_object(f,'expdisk',xc,yc,mag_d,rd,1.0,ba,theta_d,1)
+        modelPars += write_object(f,'devauc',xc,yc,mag_b,rb,4.0,ba,theta_b,2)
     else:
         raise ValueError('model %s does not exist, please choose from: sersic,expdisk, devauc or compost.')
         return None
 
-    f.close()
     f1=open('GALFIT_input','w')
-    input_file(f1,mag_zpt,sky,xsize,ysize,xsize,1,imgname="model.fits")
+    input_file(f1,modelPars,mag_zpt,sky,(1,xsize),(1,ysize),xsize,1,\
+                imgname="none",outname="model.fits",psfname=psfname)
     f1.close()
     sp.call('galfit -o1 GALFIT_input >> galfit.log',shell=True)
     galaxy=pyfits.getdata('model.fits')
-    sp.call('rm model.fits galfit_object.temp GALFIT_input galfit.log',shell=True)
+    sp.call('rm model.fits GALFIT_input galfit.log',shell=True)
 
     return galaxy
 
+def write_gaussian(xc,yc,mag,fwhm,axis_ratio,theta):
+    gaussPars = ""
+    gaussPars+= "0) gaussian           # object type\n"
+    gaussPars+= "1) %.4f  %.4f  1 1  # position x, y        [pixel]\n"%(xc,yc)
+    gaussPars+= "3) %.4f      1       # total magnitude\n"%(mag)
+    gaussPars+= "4) %.4f        1       #   FWHM               [pixels]\n"%(fwhm)
+    gaussPars+= "9) %.4f       1       # axis ratio (b/a)\n"%(axis_ratio)
+    gaussPars+= "10) %.4f         1       # position angle (PA)  [Degrees: Up=0, Left=90]\n"%(theta)
+    gaussPars+= "Z) 0                  # leave in [1] or subtract [0] this comp from data?\n"
+    return gaussPars
 
-def make_psf(mzpt,FWHM=3):
+def make_psf(mzpt,FWHM=3,psfname="psf.fits"):
+    r"""
 
+    Parameters
+    ----------
 
+    Returns
+    -------
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
     fwhm_pix = FWHM
     imsize = int(20 * fwhm_pix)
 
 
     xc,yc=imsize/2.0+1,imsize/2.0+1
-
-    f=open('galfit_object.temp','w')
-
-    f.write('0) gaussian           # object type\n')
-    f.write('1) %.2f  %.2f  1 1  # position x, y        [pixel]\n'%(xc,yc))
-    f.write('3) 14.0       1       # total magnitude\n')
-    f.write('4) %.4f        0       #   FWHM               [pixels]\n'%(fwhm_pix))
-    f.write('9) 1.0        1       # axis ratio (b/a)\n')
-    f.write('10) 0.0         1       # position angle (PA)  [Degrees: Up=0, Left=90]\n')
-    f.write('Z) 0                  # leave in [1] or subtract [0] this comp from data?\n')
-    f.close()
-    root=os.getcwd()
+    psfPars = write_gaussian(xc,yc,14.0,fwhm_pix,1.0,0.0)
 
     f=open('galfit_psf.txt','w')
-    input_file(f,mzpt,0.0,imsize,imsize,0,1,imgname='psf',maskname=False)
+    input_file(f,psfPars,mzpt,0.0,(1,imsize),(1,imsize),0,1,imgname="none",outname=psfname)
     f.close()
 
     sp.call('galfit -o1 galfit_psf.txt >> galfit.log',shell=True)
-    sp.call('rm galfit_object.temp galfit_psf.txt galfit.log',shell=True)
-
-    return
+    sp.call('rm galfit_psf.txt galfit.log',shell=True)
+    return pyfits.getdata("psf.fits")
 
 def read_results_file(fname):
+    r""" Reads a galfit HDU cube to get the results for the best fit model,
+    which are stored in the header of the cube extension 2. It is assumed that
+    a single sersic model was fit.
+
+    Parameters
+    ----------
+    fname : str
+        The name of the result file to read the model parameters from.
+
+    Returns
+    -------
+    xc : str
+        The model X center and its error (separated by whitespace)
+    yc : str
+        The model Y center and its error (separated by whitespace)
+    M : str
+        The model magnitude and its error (separated by whitespace)
+    R : str
+        The model effective radius and its error (separated by whitespace)
+    N : str
+        The model sersic index and its error (separated by whitespace)
+    Q : str
+        The model axis ratio and its error (separated by whitespace)
+    T : str
+        The model position angle and its error (separated by whitespace)
+    chi : str
+        The reduced chi square value of the fit
+    F : str
+        A galfit flag indicating the final status of the fit. If 0 everything
+        is ok. If 1 it means that GALFIT finished 100 iterations without
+        converging. If 2, it means GALFIT results diverged and they cannot
+        be trusted,
+
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
     try:
         hdu=pyfits.open(fname)
         chi=hdu[2].header['CHI2NU']
